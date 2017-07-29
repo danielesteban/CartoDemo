4ca2a28a8e67d0a929431027a9899052d4a17b8b
Daniel Esteban Nombela <dani@gatunes.com>
2017-07-29 21:14:49 +0200

    Touch support +firefox mousewheel

--- src/Input.js
+++ src/Input.js
@@ -1,4 +1,5 @@
 import { vec2 } from 'gl-matrix';
+import touches from 'touches';
 
 class Input {
   constructor(renderer) {
@@ -6,36 +7,42 @@ class Input {
     this.button = -1;
     this.mouseX = -1;
     this.mouseY = -1;
-    window.addEventListener('mousedown', this.onMouseDown.bind(this));
-    window.addEventListener('mousemove', this.onMouseMove.bind(this));
-    window.addEventListener('mouseup', this.onMouseUp.bind(this));
+    this.wheelDebounce = 0;
+    touches(window, { filtered: true })
+      .on('start', this.onMouseDown.bind(this))
+      .on('move', this.onMouseMove.bind(this))
+      .on('end', this.onMouseUp.bind(this));
     window.addEventListener('mousewheel', this.onMouseWheel.bind(this));
+    window.addEventListener('wheel', this.onMouseWheel.bind(this));
     window.addEventListener('keydown', this.onKeyDown.bind(this));
   }
-  onMouseDown({ button, clientX, clientY }) {
-    this.button = button;
-    this.mouseX = clientX;
-    this.mouseY = clientY;
+  onMouseDown({ button }, pos) {
+    this.button = button || 0;
+    this.mouseX = pos[0];
+    this.mouseY = pos[1];
   }
-  onMouseMove({ clientX, clientY }) {
+  onMouseMove(e, pos) {
     if (this.button !== 0) return;
     const { renderer } = this;
     const { center, scale } = renderer;
     const diff = vec2.fromValues(
-      (this.mouseX - clientX) * scale,
-      (clientY - this.mouseY) * scale
+      (this.mouseX - pos[0]) * scale,
+      (pos[1] - this.mouseY) * scale
     );
     renderer.setCenter(vec2.add(vec2.create(), center, diff));
-    this.mouseX = clientX;
-    this.mouseY = clientY;
+    this.mouseX = pos[0];
+    this.mouseY = pos[1];
   }
   onMouseUp() {
     this.button = -1;
   }
-  onMouseWheel({ wheelDelta }) {
-    const { renderer } = this;
+  onMouseWheel({ deltaY }) {
+    const { renderer, wheelDebounce: debounce } = this;
     const { center, scale, render3D } = renderer;
-    const step = wheelDelta > 0 ? 0.5 : 2;
+    const now = Date.now();
+    const step = deltaY > 0 ? 2 : 0.5;
+    if (debounce > now) return;
+    this.wheelDebounce = now + 100;
     renderer.setScale(Math.min(Math.max(scale * step, 0.000001), 0.0001));
     if (render3D) renderer.setCenter(center);
   }
