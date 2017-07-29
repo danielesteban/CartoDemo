26703f2eeb1d8ec30062704113dffd791d557c51
Daniel Esteban Nombela <dani@gatunes.com>
2017-07-29 20:07:51 +0200

    Let the user know about the keyboard shortcuts

--- src/Status.js
+++ src/Status.js
@@ -2,13 +2,25 @@ class Status {
   constructor() {
     this.dom = document.createElement('status');
     document.body.appendChild(this.dom);
+    this.help = document.createElement('help');
+    [
+      { key: 'W', action: 'Wireframe rendering' },
+      { key: 'P', action: 'Experimental 3D view' },
+    ].forEach(({ key, action }) => {
+      const p = document.createElement('p');
+      p.innerText = `[${key}] ${action}`;
+      this.help.appendChild(p);
+    });
+    document.body.appendChild(this.help);
   }
   update(text) {
     if (text) {
       this.dom.innerText = text;
       this.dom.style.opacity = 1;
+      this.help.style.opacity = 0;
     } else {
       this.dom.style.opacity = 0;
+      this.help.style.opacity = 1;
     }
   }
 }
